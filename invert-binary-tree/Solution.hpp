e4fdbe266442d09c2696fabfc8ead34dca9a11c3
Peter Wilson <wilsonpe66@gmail.com>
2024-04-01 16:11:11 -0400

    Added invert-binary-tree

---
+++ invert-binary-tree/Solution.hpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/invert-binary-tree/submissions/1220465945/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    constexpr static TreeNode* invertTree(TreeNode* root) {
+        if (!root) return nullptr;
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
