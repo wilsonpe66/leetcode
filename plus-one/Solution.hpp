2341de00db8186ce759a989eae6df23c1aac5e6d
Peter Wilson <wilsonpe66@gmail.com>
2024-04-01 14:05:09 -0400

    added plus-one

---
+++ plus-one/Solution.hpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/plus-one/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+public:
+    vector<int> plusOne(const vector<int>& digits) {
+        if (digits.empty()) return digits;
+        vector<int> result = digits;
+
+        auto it = result.rbegin();
+        int sum = *it + 1;
+        int carry = (sum > 9);
+        *it = sum - 10 * carry;
+        for (++it; it != result.rend(); ++it) {
+            int sum = *it + carry;
+            carry = (sum > 9);
+            *it = sum - 10 * carry;
+        }
+
+        if (carry) result.insert(result.begin(), carry);
+        return result;
+    }
+};
