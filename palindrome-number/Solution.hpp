467d7144577f2eaa12ed08fe080e7c8700923cfc
Peter Wilson <wilsonpe66@gmail.com>
2024-04-07 22:03:07 -0400

    Optimized palindrome-number

--- palindrome-number/Solution.hpp
+++ palindrome-number/Solution.hpp
@@ -1,20 +1,15 @@
-// https://leetcode.com/problems/palindrome-number/
-
 class Solution {
-    constexpr static long reverse_positive(long x) {
-        const long sign = 1L - ((x < 0L)<<1);
-        x *= sign;
+public:
+    static constexpr bool isPalindrome(long in_x) {
+        if ( in_x < 0 ) return false;
         long result = 0L;
-        while (x) {
-            result = (10L*result) + (x%10L);
-            x /= 10L;
+        long new_x = 0L;
+        for (auto x = in_x; x; x = new_x) {
+            new_x = x / 10L;
+            result = 10L * (result - new_x) + x; 
         }
 
-        return (INT_MAX >= result) * result * sign;
-    }
-
-public:
-    static constexpr inline bool isPalindrome(long x) {
-        return ( x >= 0 ) && reverse_positive(x) == x;
+        return result ==  in_x;
     }
 };
+
