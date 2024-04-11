aef3ca6ba970dfb764cd16ba9ed7404609bedb3d
Peter Wilson <wilsonpe66@gmail.com>
2024-04-11 14:02:35 -0400

    Optimized construct-quad-tree

--- construct-quad-tree/Solution.hpp
+++ construct-quad-tree/Solution.hpp
@@ -41,37 +41,59 @@ public:
 */
 
 class Solution {
-    class GridView {
+    static constexpr inline const size_t pool_cap = 128 * 128;
+    static inline array<Node, pool_cap> pool;
+    static inline size_t pool_sz = 0;
+
+    template<typename K>
+    static inline void pp(K && k) {
+        cout<<k;
+        cout<<endl;
+    }
+
+    template<typename K, typename... T>
+    static inline void pp(K && k, T && ...t) {
+        cout<<k<<" ";
+        pp(t...);
+    }
+
+    template<typename... T>
+    static inline Node * allocate_node(const T && ...t) {
+        if (pool_sz < pool_cap) {
+            return new (&pool[pool_sz++]) Node(t...);
+        }
+        return new Node(t...);
+    }
+
+    class SqrPow2GridView {
         const vector<vector<int>>* pgrid;
         size_t top_left_row_;
         size_t top_left_col_;
-        size_t num_of_rows_;
-        size_t num_of_cols_;
+        size_t length_of_sides_;
 
-        constexpr GridView(const vector<vector<int>>& grid, const size_t top_left_row, const size_t top_left_col, const size_t num_of_rows,
-                                const size_t num_of_cols): 
-            pgrid{&grid}, top_left_row_{top_left_row}, top_left_col_{top_left_col},
-            num_of_rows_(num_of_rows), num_of_cols_(num_of_cols)
+        constexpr SqrPow2GridView(const vector<vector<int>>& grid, const size_t top_left_row, const size_t top_left_col, const size_t length_of_sides): 
+            pgrid{&grid}, top_left_row_{top_left_row}, top_left_col_{top_left_col}, length_of_sides_(length_of_sides)
             {}
 
     public:
-        explicit constexpr GridView(const vector<vector<int>>& grid): 
-            pgrid{&grid}, top_left_row_{0L}, top_left_col_{0L},
-            num_of_rows_(grid.size()), num_of_cols_(grid[0L].size())
+        explicit constexpr SqrPow2GridView(const vector<vector<int>>& grid): 
+            pgrid{&grid}, top_left_row_{0L}, top_left_col_{0L}, length_of_sides_(grid.size())
             {}
 
         void print() const {
             cout<<"GridView:"
                 "\n\ttop_left_row="<<top_left_row_
-                <<" top_left_col="<<top_left_col_
-                <<"\n\tnum_of_rows="<<num_of_rows_
-                <<" num_of_cols="<<num_of_cols_
+                <<"\ttop_left_col="<<top_left_col_
+                <<"\n\tlength_of_sides_="<<length_of_sides_
                 <<"\n";
 
-            for(size_t row = 0L; row  < num_of_rows_; ++row) {
+            const size_t end_row = length_of_sides_ + top_left_row_;
+            const size_t end_col = length_of_sides_ + top_left_col_;
+            for(size_t row = top_left_row_; row != end_row; ++row) {
+                const auto &current_row = (*pgrid)[row];
                 cout<< "\t|";
-                for(size_t col = 0L; col  < num_of_cols_; ++col) {
-                    cout<< " "<< get(row, col);
+                for(size_t col = top_left_col_; col != end_col; ++col) {
+                    cout<< " "<< current_row[col];
                 }
                 cout<< " |\n";
             }
@@ -79,17 +101,18 @@ class Solution {
         }
 
         constexpr bool empty() const {
-            return !num_of_rows_ || !num_of_cols_;
+            return !length_of_sides_;
         }
 
         constexpr bool isAllSame() const {
             const int val = (*pgrid)[top_left_row_][top_left_col_];
             
-            const size_t num_of_rows = num_of_rows_ + top_left_row_;
-            const size_t num_of_cols = num_of_cols_ + top_left_col_;
-            for(size_t row = top_left_row_; row != num_of_rows; ++row) {
-                for(size_t col = top_left_col_; col != num_of_cols; ++col) {
-                    if ((*pgrid)[row][col] != val) return false;
+            const size_t end_row = length_of_sides_ + top_left_row_;
+            const size_t end_col = length_of_sides_ + top_left_col_;
+            for(size_t row = top_left_row_; row != end_row; ++row) {
+                const auto &current_row = (*pgrid)[row];
+                for(size_t col = top_left_col_; col != end_col; ++col) {
+                    if (current_row[col] != val) return false;
                 }
             }
             return true;
@@ -99,58 +122,33 @@ class Solution {
             return (*pgrid)[row + top_left_row_][col + top_left_col_];
         }
 
-        constexpr inline int num_of_rows() const { return num_of_rows_; }
-        constexpr inline int num_of_cols() const { return num_of_cols_; }
+        constexpr inline int length_of_sides() const { return length_of_sides_; }
 
-        constexpr optional<GridView> subView(const size_t top_left_row, const size_t top_left_col, const size_t num_of_rows,
-                                             const size_t num_of_cols) const {
-            if (num_of_rows + top_left_row > num_of_rows_ || num_of_cols + top_left_col > num_of_cols_) return {};
-
-            return GridView(
+        constexpr inline SqrPow2GridView subView(const size_t top_left_row, const size_t top_left_col, const size_t length_of_sides) const {
+            return SqrPow2GridView(
                 *pgrid,
                 top_left_row_ + top_left_row,
                 top_left_col_ + top_left_col,
-                num_of_rows,
-                num_of_cols
-            );
-        }
-
-        constexpr optional<GridView> subView(const size_t top_left_row, const size_t top_left_col) const {
-            const size_t top_left_row_local = top_left_row_ + top_left_row;
-            const size_t top_left_col_local = top_left_col_ + top_left_col;
-
-            if (top_left_row > num_of_rows_ || top_left_col > num_of_cols_) return {};
-
-            return GridView(
-                *pgrid,
-                top_left_row_local,
-                top_left_col_local,
-                num_of_rows_ - top_left_row,
-                num_of_cols_ - top_left_col
+                length_of_sides
             );
         }
     };
-
-    static inline Node* construct(optional<GridView> opt_grid_view) {
-        return opt_grid_view ? construct(*opt_grid_view): nullptr;
-    }
 
     /**
         n == grid.length == grid[i].length
         n == 2^x where 0 <= x <= 6
      */
-    static Node* construct(GridView grid_view) {
+    static Node* construct(SqrPow2GridView grid_view) {
         if (grid_view.empty()) return nullptr;
 
-        const size_t mid_row = grid_view.num_of_rows()>>1;
-        const size_t mid_col = grid_view.num_of_cols()>>1;
-        if (!mid_row || !mid_col || grid_view.isAllSame()) return new Node(grid_view.get(0, 0), true);
+        const size_t mid = grid_view.length_of_sides()>>1;
+        if (!mid || grid_view.isAllSame()) return allocate_node(grid_view.get(0, 0), true);
 
-        return new Node(false, false, 
-            construct(grid_view.subView(0, 0, mid_row, mid_col)),
-            construct(grid_view.subView(0, mid_col, mid_row, mid_col)),
-            construct(grid_view.subView(mid_row, 0, mid_row, mid_col)),
-            construct(grid_view.subView(mid_row, mid_col, mid_row, mid_col))
+        return allocate_node(false, false, 
+            construct(grid_view.subView(0, 0, mid)),
+            construct(grid_view.subView(0, mid, mid)),
+            construct(grid_view.subView(mid, 0, mid)),
+            construct(grid_view.subView(mid, mid, mid))
         );
     }
 
@@ -160,7 +158,7 @@ public:
         n == 2^x where 0 <= x <= 6
      */
     static Node* construct(const vector<vector<int>>& grid) {
-        return construct(GridView(grid));
+        return construct(SqrPow2GridView(grid));
     }
 };
 
